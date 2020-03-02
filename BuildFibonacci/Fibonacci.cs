dd8ae83e660917afec565fda00c60daec599ce6a
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-02 13:58:17 -0700

    Add project files.

---
+++ BuildFibonacci/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFibonacci
+{
+    public class Fibonacci
+    {
+        public Fibonacci()
+        {
+
+        }
+
+        // Creates 20 fibonacci numbers based on the parameters.
+        public void printSequence(int first, int second)
+        {
+            List<int> sequence = new List<int>() { first, second };
+
+            Console.WriteLine("\nFirst integer: {0} | Second integer: {1}", first, second);
+
+            for (int i = 1; i < 21; i++)
+            {
+                int newValue = sequence[i] + sequence[i - 1];
+                Console.Write("{0,-8}  ", newValue);
+                sequence.Add(newValue);
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BuildFibonacci/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace BuildFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program creates a Fibonacci sequence from two numbers.");
+            System.Threading.Thread.Sleep(750);
+
+            Fibonacci fib = new Fibonacci();
+
+            bool loop = true;
+            while (loop)
+            {
+                int num1 = 0;
+                // Get number 1
+                bool badNumber1 = true;
+                while (badNumber1)
+                {
+                    Console.Write("Enter integer 1:  ");
+                    try
+                    {
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        badNumber1 = false;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Enter an INTEGER, please...\n");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unknown exception: {0}", e.GetType());
+                    }
+                }
+
+                System.Threading.Thread.Sleep(150);
+
+                int num2 = 0;
+                // Get number 2
+                bool badNumber2 = true;
+                while (badNumber2)
+                {
+                    Console.Write("Enter integer 2:  ");
+                    try
+                    {
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        badNumber2 = false;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Enter an INTEGER, please...\n");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unknown exception: {0}", e.GetType());
+                    }
+                }
+
+                // Making the Fibonacci sequence
+                fib.printSequence(first: num1, second: num2);
+
+                // Go again?
+                Console.Write("Do you want to enter new numbers? (y or n) ");
+                string loopChoice = Console.ReadLine();
+                if (loopChoice != "y")
+                {
+                    loop = false;
+                }
+            }
+
+            Console.WriteLine("See ya later!");
+            Console.ReadLine();
+        }
+    }
+}
